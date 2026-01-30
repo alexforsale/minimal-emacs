c8958582bff19720bc8b83b623fbf111623786b4
alexforsale <alexforsale@yahoo.com>
2026-01-31 02:14:18 +0700

    add early-init.el and init.el

---
+++ early-init.el
@@ -0,0 +1,23 @@
+(customize-set-variable 'custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+(add-to-list 'default-frame-alist '(menu-bar-lines . 0))
+(add-to-list 'initial-frame-alist '(menu-bar-lines . 0))
+
+(add-to-list 'initial-frame-alist '(tool-bar-lines . 0))
+(add-to-list 'default-frame-alist '(tool-bar-lines . 0))
+
+(add-to-list 'initial-frame-alist '(vertical-scroll-bars))
+(add-to-list 'default-frame-alist '(vertical-scroll-bars))
+
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(with-eval-after-load 'package
+  (setopt package-enable-at-startup nil)
+  ;; Add `melpa` to `package-archives`.
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/") t)
+  ;; gnu-devel
+  (add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/") t)
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t))
---
+++ init.el
@@ -0,0 +1,37 @@
+(cond ((file-directory-p (expand-file-name "Sync/org" (getenv "HOME")))
+       (customize-set-variable '+config/org-directory (expand-file-name "Sync/org" (getenv "HOME"))))
+      ((string-match-p "microsoft" (shell-command-to-string "uname -a"))
+       (if (file-directory-p "/mnt/c/Users/SyncthingServiceAcct/Default Folder/org")
+           (customize-set-variable '+config/org-directory "/mnt/c/Users/SyncthingServiceAcct/Default Folder/org"))))
+(use-package emacs
+  :ensure nil
+  :config
+  (delete-selection-mode 1)
+  (transient-mark-mode 1)
+  (setopt use-short-answers t))
+
+(use-package which-key
+  :ensure nil
+  :init
+  (which-key-mode 1))
+
+(use-package files
+  :ensure nil
+  :config
+  (auto-save-visited-mode 1)
+  (nconc
+   auto-mode-alist
+   '(("/LICENSE\\'" . text-mode)
+     ("\\.log\\'" . text-mode)
+     ("rc\\'" . conf-mode)
+     ("\\.\\(?:hex\\|nes\\)\\'" . hexl-mode)))
+  (setopt auto-save-visited-interval 10
+	  revert-without-query (list ".")
+	  find-file-suppress-same-file-warnings t
+	  find-file-visit-truename t
+	  confirm-kill-processes nil
+	  version-control t
+	  backup-by-copying t
+	  backup-directory-alist `(("." . ,(expand-file-name ".backup" user-emacs-directory))))
+  :hook
+  ((prog-mode text-mode) . auto-save-visited-mode))
